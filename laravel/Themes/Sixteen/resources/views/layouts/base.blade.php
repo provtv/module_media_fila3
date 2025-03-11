1475289be524ff82e3a66c51bad1fdb761c9f16d
marco76tv <marco.sottana@gmail.com>
2025-03-11 16:53:50 +0100

    .

--- laravel/Themes/Sixteen/resources/views/layouts/base.blade.php
+++ laravel/Themes/Sixteen/resources/views/layouts/base.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @hasSection('title')
-
             <title>@yield('title') - {{ config('app.name') }}</title>
         @else
             <title>{{ config('app.name') }}</title>
@@ -20,7 +19,6 @@
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
         @filamentStyles
         @vite(['resources/css/app.css'],'themes/Sixteen/dist')
-
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
