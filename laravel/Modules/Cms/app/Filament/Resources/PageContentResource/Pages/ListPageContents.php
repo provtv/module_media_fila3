cc41944965b8d6d6623396082e8206075cc45c9a
marco76tv <marco.sottana@gmail.com>
2025-03-11 15:01:47 +0100

    .

--- laravel/Modules/Cms/app/Filament/Resources/PageContentResource/Pages/ListPageContents.php
+++ laravel/Modules/Cms/app/Filament/Resources/PageContentResource/Pages/ListPageContents.php
@@ -4,20 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Resources\PageContentResource\Pages;
 
-use Filament\Actions;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Enums\ActionsPosition;
-use Filament\Tables\Enums\FiltersLayout;
-use Filament\Tables\Table;
 use Modules\Cms\Filament\Resources\PageContentResource;
-use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListPageContents extends XotBaseListRecords
@@ -25,13 +14,6 @@ class ListPageContents extends XotBaseListRecords
     use ListRecords\Concerns\Translatable;
 
     // protected static string $resource = PageContentResource::class;
-
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
-        ];
-    }
 
     public function getListTableColumns(): array
     {
@@ -42,67 +24,6 @@ class ListPageContents extends XotBaseListRecords
             TextColumn::make('slug')
                 ->sortable()
                 ->searchable(),
-        ];
-    }
-
-    public function getTableFilters(): array
-    {
-        return [
-        ];
-    }
-
-    public function getTableActions(): array
-    {
-        return [
-            ViewAction::make()
-                ->label(''),
-            EditAction::make()
-                ->label(''),
-            DeleteAction::make()
-                ->label('')
-                ->requiresConfirmation(),
-        ];
-    }
-
-    public function getTableBulkActions(): array
-    {
-        return [
-            DeleteBulkAction::make(),
-        ];
-    }
-
-    public function table(Table $table): Table
-    {
-        return $table
-            // ->columns($this->getTableColumns())
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-
-            ->filters($this->getTableFilters())
-            ->filtersLayout(FiltersLayout::AboveContent)
-            ->persistFiltersInSession()
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions())
-            ->actionsPosition(ActionsPosition::BeforeColumns)
-            ->defaultSort(
-                column: 'created_at',
-                direction: 'DESC',
-            );
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-            Actions\LocaleSwitcher::make(),
-        ];
-    }
-
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            // TableLayoutToggleTableAction::make(),
         ];
     }
 }
