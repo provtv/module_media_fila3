994a8979c91ea6e200abcbe7c61a146c8a66a78c
marco76tv <marco.sottana@gmail.com>
2025-03-13 18:41:51 +0100

    Squashed 'laravel/Modules/Geo/' changes from e43082ddab..3f023cff4e
    3f023cff4e Merge commit 'e43082ddab5050b29660363efb6d6b034642dab4' into dev
    2e1aa7b2c5 up
    0b305eb63e up
    f069cc0b47 Check & fix styling
    d5f06d9a31 Auto update submodule
    3abaebe3e0 Check & fix styling
    f0dd87fc1d Auto update submodule
    2391eb2ef0 Auto update submodule
    2638e1cca6 Check & fix styling
    f4d6961eec Auto update submodule
    99408bcc70 Check & fix styling
    f3adf0ad52 up
    7720a3d316 up
    2d52a038a2 up
    fce45f575a Check & fix styling
    47701a72ae Auto update submodule
    f0b191395d Check & fix styling
    8942d51265 up
    fc9b14c8f0 feat(ListLocations): add search functionality to columns in the list table feat(ListLocations): add 'name', 'street', 'city', 'state', and 'zip' columns to the list table for filtering and sorting
    b1f4910770 up
    b51b63f12b Merge commit 'f7b6efa26a350458318c0325df2248902561c774' into dev
    
    git-subtree-dir: laravel/Modules/Geo
    git-subtree-split: 3f023cff4e02f95f2359b47f5614373a63aed115

---
+++ app/Datas/Photon/PhotonPropertiesData.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Geo\Datas\Photon;
+
+use Spatie\LaravelData\Data;
+
+class PhotonPropertiesData extends Data
+{
+    public function __construct(
+        public ?string $country,
+        public ?string $city,
+        public ?string $postcode,
+        public ?string $street,
+        public ?string $housenumber,
+    ) {
+    }
+}
