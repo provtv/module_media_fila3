93dd97b781d971732433d8b346637276d1abdf42
marco76tv <marco.sottana@gmail.com>
2025-03-13 19:14:37 +0100

    .

---
+++ laravel/Modules/Fixcity/lang/it/edit_ticket.php
@@ -0,0 +1,48 @@
+<?php return array (
+  'fields' => 
+  array (
+    'category_id' => 
+    array (
+      'label' => 'category_id',
+    ),
+    'title' => 
+    array (
+      'label' => 'title',
+    ),
+    'content' => 
+    array (
+      'label' => 'content',
+    ),
+    'status' => 
+    array (
+      'label' => 'status',
+    ),
+    'priority' => 
+    array (
+      'label' => 'priority',
+    ),
+    'attachments' => 
+    array (
+      'label' => 'attachments',
+    ),
+    'type' => 
+    array (
+      'label' => 'type',
+    ),
+    'images' => 
+    array (
+      'label' => 'images',
+    ),
+  ),
+  'actions' => 
+  array (
+    'cancel' => 
+    array (
+      'label' => 'cancel',
+    ),
+    'save' => 
+    array (
+      'label' => 'save',
+    ),
+  ),
+);
