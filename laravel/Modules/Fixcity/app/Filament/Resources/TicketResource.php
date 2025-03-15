4726ccb1d2b952bc5ddbc87c09ee4f0eee3f0588
kristiRepository <kristinano6346@gmail.com>
2025-03-15 02:18:53 +0100

    debugging

--- laravel/Modules/Fixcity/app/Filament/Resources/TicketResource.php
+++ laravel/Modules/Fixcity/app/Filament/Resources/TicketResource.php
@@ -114,15 +114,15 @@ class TicketResource extends Resource
                             'lat' => 40.4168,
                             'lng' => -3.7038,
                         ])
-                        ->afterStateUpdated(function (Set $set, ?array $state): void {
-                            if (is_array($state)) {
-                                $set('latitude', $state['lat']);
-                                $set('longitude', $state['lng']);
-                            }
-                        })
-                        // ->afterStateHydrated(function ($state, $record, Set $set): void {
-                        //     $set('location', ['lat' => $record?->latitude ?? 0, 'lng' => $record?->longitude ?? 0]);
+                        // ->afterStateUpdated(function (Set $set, ?array $state): void {
+                        //     if (is_array($state)) {
+                        //         $set('latitude', $state['lat']);
+                        //         $set('longitude', $state['lng']);
+                        //     }
                         // })
+                        ->afterStateHydrated(function ($state, $record, Set $set): void {
+                            $set('location', ['lat' => $record?->latitude ?? 0, 'lng' => $record?->longitude ?? 0]);
+                        })
                         ->rules([new FilterCoordinatesInRadius])
                         ->liveLocation()
                         ->showMarker(true) // https://github.com/dotswan/filament-map-picker/pull/51
