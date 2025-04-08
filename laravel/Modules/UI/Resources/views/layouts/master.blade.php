46f81d702c5174c0d8e4404d17a9555f2577f161
marco76tv <marco.sottana@gmail.com>
2025-04-08 22:01:01 +0200

    .

--- laravel/Modules/UI/Resources/views/layouts/master.blade.php
+++ laravel/Modules/UI/Resources/views/layouts/master.blade.php
@@ -6,25 +6,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Module UI</title>
 
-<<<<<<<< HEAD:laravel/storage/framework/views/5f8bd7e4dcb94a35926e0e694d791a49.php
-       
-       
-========
-       {{-- Laravel Vite - CSS File --}}
-       {{-- {{ module_vite('build-ui', 'resources/assets/sass/app.scss') }} --}}
->>>>>>>> origin/dev:laravel/Modules/UI/Resources/views/layouts/master.blade.php
+        {{-- Laravel Vite - CSS File --}}
+        {{-- {{ module_vite('build-ui', 'resources/assets/sass/app.scss') }} --}}
 
     </head>
     <body>
-        <?php echo $__env->yieldContent('content'); ?>
+        @yield('content')
 
-<<<<<<<< HEAD:laravel/storage/framework/views/5f8bd7e4dcb94a35926e0e694d791a49.php
-        
-        
-========
         {{-- Laravel Vite - JS File --}}
         {{-- {{ module_vite('build-ui', 'resources/assets/js/app.js') }} --}}
->>>>>>>> origin/dev:laravel/Modules/UI/Resources/views/layouts/master.blade.php
     </body>
 </html>
 <?php /**PATH /var/www/html/_bases/base_fixcity_fila3/laravel/Modules/AI/resources/views/layouts/master.blade.php ENDPATH**/ ?>
