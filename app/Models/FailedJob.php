6b69f088ef8602edcbbdabe70150b5f55b03240b
marco76tv <marco.sottana@gmail.com>
2025-04-10 14:15:41 +0200

    Squashed 'laravel/Modules/Job/' content from commit ace82f2d7
    git-subtree-dir: laravel/Modules/Job
    git-subtree-split: ace82f2d7d7e6cebd8abe3c1d9ad0bee0320b4ba

---
+++ app/Models/FailedJob.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * ---.
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Job\Models;
+
+/**
+ * Modules\Job\Models\FailedJob.
+ *
+ * @method static \Modules\Job\Database\Factories\FailedJobFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob query()
+<<<<<<< HEAD
+=======
+ *
+>>>>>>> origin/dev
+ * @property int $id
+ * @property string $uuid
+ * @property string $connection
+ * @property string $queue
+ * @property array $payload
+ * @property string $exception
+ * @property string $failed_at
+<<<<<<< HEAD
+=======
+ *
+>>>>>>> origin/dev
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob whereConnection($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob whereException($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob whereFailedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob wherePayload($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob whereQueue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJob whereUuid($value)
+<<<<<<< HEAD
+ * @property \Modules\Xot\Contracts\ProfileContract|null $creator
+ * @property \Modules\Xot\Contracts\ProfileContract|null $updater
+=======
+ *
+ * @property \Modules\Xot\Contracts\ProfileContract|null $creator
+ * @property \Modules\Xot\Contracts\ProfileContract|null $updater
+ *
+>>>>>>> origin/dev
+ * @mixin \Eloquent
+ */
+class FailedJob extends BaseModel
+{
+    protected $fillable = [
+        'id',
+        'uuid',
+        'connection',
+        'queue',
+        'payload',
+        'exception',
+        'failed_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'uuid' => 'string',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+
+            'updated_by' => 'string',
+            'created_by' => 'string',
+            'deleted_by' => 'string',
+
+            'payload' => 'array',
+        ];
+    }
+}
