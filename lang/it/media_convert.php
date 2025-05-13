22c7ea402c3447b702c41ff132cc823b5a9e0b52
marco76tv <marco.sottana@gmail.com>
2025-05-13 08:45:02 +0200

    .

--- lang/it/media_convert.php
+++ lang/it/media_convert.php
@@ -1,10 +1,22 @@
 <?php
 
-return [
-    'navigation' => [
-        'label' => 'media convert',
-        'group' => 'media convert',
-        'icon' => 'media convert',
-        'sort' => 20,
-    ],
-];
+return array (
+  'navigation' => 
+  array (
+    'label' => 'media convert',
+    'group' => 'media convert',
+    'icon' => 'media convert',
+    'sort' => 20,
+  ),
+  'fields' => 
+  array (
+    'applyFilters' => 
+    array (
+      'label' => 'applyFilters',
+    ),
+    'toggleColumns' => 
+    array (
+      'label' => 'toggleColumns',
+    ),
+  ),
+);
