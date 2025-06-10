e8846ec77d3a64402124503174fc402ea8ccd085
marco76tv <marco.sottana@gmail.com>
2025-06-10 12:09:43 +0200

    .

---
+++ app/Filament/Tables/Columns/IconMediaColumn.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Media\Filament\Tables\Columns;
+
+use Exception;
+use Illuminate\Support\Arr;
+use Spatie\ModelStates\State;
+use Modules\SaluteOra\Models\User;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\IconColumn;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\SelectColumn;
+
+class IconMediaColumn extends IconColumn
+{
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $attachment=$this->getName();
+
+        $this->default(fn($record)=>$record->getFirstMedia($attachment))
+                ->icon('heroicon-o-document-text')
+                ->color(fn ($record) => $record->getFirstMedia($attachment) ? 'success' : 'danger')
+                ->tooltip(fn ($record) => $record->getFirstMedia($attachment)?->file_name ?? 'Documento non caricato')
+
+                ->action(function ($record) use ($attachment) {
+                    $media = $record->getFirstMedia($attachment);
+                    if (!$media) {
+                        return;
+                    }
+
+                    return Storage::disk($media->disk)->download($media->getPathRelativeToRoot());
+
+                    //return response()->streamDownload(function () use ($media) {
+                    //    echo $media->get();
+                    //}, $media->file_name);
+
+                })
+                ;
+
+
+    }
+
+
+
+
+}
---
+++ lang/it/icon_media.php
@@ -0,0 +1,25 @@
+<?php
+
+return array (
+  'fields' => 
+  array (
+    'change-state' => 
+    array (
+      'label' => 'change-state',
+    ),
+    'state' => 
+    array (
+      'label' => 'state',
+      'placeholder' => 'state',
+      'helper_text' => 'state',
+      'description' => 'state',
+    ),
+    'message' => 
+    array (
+      'label' => 'message',
+      'placeholder' => 'message',
+      'helper_text' => 'message',
+      'description' => 'message',
+    ),
+  ),
+);
