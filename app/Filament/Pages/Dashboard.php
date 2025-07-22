2fa19025cf25a2b03ea55f90b18dcf5c6019be90
marco76tv <marco.sottana@gmail.com>
2025-07-22 09:28:49 +0200

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -9,6 +9,5 @@ use Filament\Pages\Page;
 class Dashboard extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-home';
-
     protected static string $view = 'media::filament.pages.dashboard';
 }
