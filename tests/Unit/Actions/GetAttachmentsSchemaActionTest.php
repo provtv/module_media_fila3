8749ada8aac861f303fb24e0677d9cc712342f4d
Malebestia <malebestia@yahoo.it>
2025-08-26 17:45:58 +0200

    .

--- tests/Unit/Actions/GetAttachmentsSchemaActionTest.php
+++ tests/Unit/Actions/GetAttachmentsSchemaActionTest.php
@@ -256,3 +256,7 @@ class GetAttachmentsSchemaActionTest extends TestCase
         $this->assertStringContainsString('Select invoice file', $component->getPlaceholder());
     }
 }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 4c4fdfa (.)
